dabe03ea03bd187b94facaa0e46ca2c326e28fa5
Nikolai Mavrenkov <koluch@koluch.ru>
2016-01-23 11:28:01 +0300

    New DateTimePicker: implement basic logic

--- src/scripts/DateTimePicker.jsx
+++ src/scripts/DateTimePicker.jsx
@@ -9,28 +9,126 @@ import DropDownContainer from './DropDownContainer'
 
 const DateTimePickerModal = React.createClass({
 
-    render: function() {
-
+    getInitialState: function() {
         const timestamp = 1453474169224
+        var m = _moment(timestamp);
+
+        return {
+            timestamp: timestamp,
+            year: m.year(),
+            month: m.month(),
+            //week: m.week(),
+            //day: m.day(),
+        }
+    },
+
+    onChangeTime: function(year, month, week, day) {
+
+        console.log(year, month, week, day);
+
+        const {timestamp} = this.state;
+
+        const m = _moment(timestamp)
+        m.year(year)
+        m.month(month)
+        m.week(week)
+        m.day(day)
+
+        this.setState(update(this.state, {
+            timestamp: {$set: m.valueOf()},
+        }))
+    },
+
+    onPrevMonth: function() {
+        const {year, month} = this.state;
+        const m = _moment(1);
+        m.year(year)
+        m.month(month)
+        m.subtract(1, "month");
+
+        this.setState(update(this.state, {
+            month: {$set: m.month()},
+        }))
+    },
+
+    onNextMonth: function() {
+        const {year, month} = this.state;
+        const m = _moment(1);
+        m.year(year)
+        m.month(month)
+        m.add(1, "month");
+
+        this.setState(update(this.state, {
+            month: {$set: m.month()},
+        }))
+    },
+
+    onPrevYear: function() {
+        const {year, month} = this.state;
+        const m = _moment(1);
+        m.year(year)
+        m.month(month)
+        m.subtract(1, "year");
+
+        this.setState(update(this.state, {
+            year: {$set: m.year()},
+        }))
+    },
+
+    onNextYear: function() {
+        const {year, month} = this.state;
+        const m = _moment(1);
+        m.year(year)
+        m.month(month)
+        m.add(1, "year");
+
+        this.setState(update(this.state, {
+            year: {$set: m.year()},
+        }))
+    },
+
+
+    render: function() {
+        console.log(this.state);
+
+        const {timestamp, year, month} = this.state;
 
         const days = [0,1,2,3,4,5,6]
         const weeks = [0,1,2,3,4,5]
 
-        var m = _moment(timestamp);
 
-        m.date(1)
-        m.subtract('days', m.day())
-        var start = m.valueOf();
+        var now = _moment(timestamp);
+
+        console.log(now.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+        var showing = _moment(1);
+
+        showing.year(year)
+        showing.month(month)
+        showing.date(1)
+
+        var mStart = _moment(showing.valueOf());
+        mStart.subtract(mStart.day(), 'days')
+        var start = mStart.valueOf();
 
         return (
             <div>
-                <h1>{m.format("dddd, MMMM Do YYYY (dddd), h:mm:ss a") + " - " + m.day()}</h1>
+                <h3>
+                    <button type="button" onClick={(e) => this.onPrevYear()}> left </button>
+                    {showing.format("YYYY")}
+                    <button type="button" onClick={(e) => this.onNextYear()}> right </button>
+                </h3>
+                <h3>
+                    <button type="button" onClick={(e) => this.onPrevMonth()}> left </button>
+                    {showing.format("MMMM")}
+                    <button type="button" onClick={(e) => this.onNextMonth()}> right </button>
+                </h3>
                 <table>
                     <thead>
                         <tr>
                             {
-                                days.map(dayI => (
-                                    <td key={dayI}>{dayI}</td>
+                                days.map(day => (
+                                    <td key={"header_day_" + day}>{_moment(start).day(day).format("ddd")}</td>
                                 ))
                             }
                         </tr>
@@ -41,11 +139,19 @@ const DateTimePickerModal = React.createClass({
                             weeks.map(week => (
                                 <tr key={"week_" + week}>
                                     {
-                                        days.map(day => (
-                                            <td key={"week_"+week+"day_" + day}>
-                                                {_moment(start).add('weeks', week).add('days', day).format("DD")}
-                                            </td>
-                                        ))
+                                        days.map(day => {
+                                            var date = _moment(start).add(week, 'weeks').add(day, 'days');
+                                            return (
+                                                <td key={"week_"+week+"_day_" + day}
+                                                    onClick={(e) => this.onChangeTime(date.year(), date.month(), date.week(), date.day())}>
+                                                    {
+                                                        (date.week() === now.week() && date.day() == now.day() && date.year() === now.year() && date.month() === now.month())
+                                                        ? "[" + date.format("D") + "]"
+                                                        : date.format("D")
+                                                    }
+                                                </td>
+                                            )
+                                        })
                                     }
                                 </tr>
                             ))
