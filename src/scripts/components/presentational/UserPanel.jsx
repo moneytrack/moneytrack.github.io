ae13397efd6d73e821c97047a3ce7ce5f563c4d2
Nikolai Mavrenkov <koluch@koluch.ru>
2016-02-07 15:49:03 +0300

    Fix logout

--- src/scripts/components/presentational/UserPanel.jsx
+++ src/scripts/components/presentational/UserPanel.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const UserPanel = React.createClass({
 
     render: function() {
-        return  <div className="user-panel"><a href="/logout">Logout</a></div>
+        return  <div className="user-panel"><a href={window.context.backend_url + "/logout?redirect=" + encodeURIComponent(window.location.href)}>Log out</a></div>
     },
 })
 
